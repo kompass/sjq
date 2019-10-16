f952db00166cbe3153e522816ed0ffff43f1d385
Nicolas Kirchner <nicolas.kirchner@protonmail.com>
2019-10-16 12:09:36 +0000

    Start to use ElasticBufferedStream

--- src/unicode_stream.rs
+++ src/unicode_stream.rs
@@ -1,66 +1,25 @@
-use std::io::{BufReader, Bytes, Read};
-use std::iter::Iterator;
-use unicode_reader::CodePoints;
+use std::io::Read;
+use combine_elastic_buffered_stream::ElasticBufferedReadStream;
 
-use combine::error::UnexpectedParse;
-use combine::stream::buffered::BufferedStream;
-use combine::stream::state::{SourcePosition, State};
-use combine::stream::{IteratorStream, Positioned, Resetable, StreamErrorFor, StreamOnce};
-
-pub struct ReadIterator<R: Read>(Option<CodePoints<Bytes<R>>>);
-
-impl<R: Read> ReadIterator<R> {
-    fn from_read(input: R) -> ReadIterator<R> {
-        ReadIterator(Some(CodePoints::from(input)))
-    }
-
-    fn from_read_buffered(input: R) -> ReadIterator<BufReader<R>> {
-        ReadIterator::from_read(BufReader::new(input))
-    }
-
-    pub fn from_read_buffered_normalized(input: R) -> ReadIterator<BufReader<R>> {
-        ReadIterator::from_read_buffered(input)
-    }
-}
-
-impl<R: Read> Iterator for ReadIterator<R> {
-    type Item = char;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(ref mut codepoints) = self.0 {
-            if let Some(next_or_read_err) = codepoints.next() {
-                if let Ok(c) = next_or_read_err {
-                    return Some(c);
-                }
-            }
-
-            self.0 = None;
-        }
-
-        None
-    }
-}
+use combine::stream::{Positioned, Resetable, StreamErrorFor, StreamOnce};
 
 pub struct ReadStream<R: Read>(
-    BufferedStream<State<IteratorStream<ReadIterator<BufReader<R>>>, SourcePosition>>,
+    ElasticBufferedReadStream<R>,
 );
 
 impl<R: Read> ReadStream<R> {
-    pub fn from_read_buffered_normalized(input: R, buffer_size: usize) -> ReadStream<R> {
-        let char_iter = ReadIterator::from_read_buffered_normalized(input);
-
-        ReadStream(BufferedStream::new(
-            State::with_positioner(IteratorStream::new(char_iter), SourcePosition::new()),
-            buffer_size,
-        ))
+    pub fn from_read(input: R) -> ReadStream<R> {
+        ReadStream(
+            ElasticBufferedReadStream::new(input),
+        )
     }
 }
 
 impl<R: Read> StreamOnce for ReadStream<R> {
-    type Item = char;
-    type Range = char;
-    type Position = SourcePosition;
-    type Error = UnexpectedParse;
+    type Item = <ElasticBufferedReadStream<R> as StreamOnce>::Item;
+    type Range = <ElasticBufferedReadStream<R> as StreamOnce>::Range;
+    type Position = <ElasticBufferedReadStream<R> as StreamOnce>::Position;
+    type Error = <ElasticBufferedReadStream<R> as StreamOnce>::Error;
 
     fn uncons(&mut self) -> Result<Self::Item, StreamErrorFor<Self>> {
         self.0.uncons()
@@ -72,7 +31,7 @@ impl<R: Read> StreamOnce for ReadStream<R> {
 }
 
 impl<R: Read> Resetable for ReadStream<R> {
-    type Checkpoint = usize;
+    type Checkpoint = <ElasticBufferedReadStream<R> as Resetable>::Checkpoint;
 
     fn checkpoint(&self) -> Self::Checkpoint {
         self.0.checkpoint()
